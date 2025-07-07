7c1377d75f285b6cb001c2e2426956fea7d8e8fd
Colin Z. Robertson <czr@rtnl.org.uk>
2025-07-07 16:53:32 +0100

    Update music-librarian to 1.2.1

--- music-librarian.rb
+++ music-librarian.rb
@@ -3,10 +3,10 @@ class MusicLibrarian < Formula
 
   desc "Python tool for managing and organizing music libraries"
   homepage "https://github.com/czr/music-librarian"
-  url "https://github.com/czr/music-librarian/archive/refs/tags/v1.2.0.tar.gz"
+  url "https://github.com/czr/music-librarian/archive/refs/tags/v1.2.1.tar.gz"
   # Create the release in Github and regenerate with
-  # `curl https://github.com/czr/music-librarian/archive/refs/tags/v1.2.0.tar.gz | sha256sum`
-  sha256 "af1c46c88f087bc602c600845d6a6c0cbe0ff67321a850e4f041b3f193fde860"
+  # `curl https://github.com/czr/music-librarian/archive/refs/tags/vX.Y.Z.tar.gz | sha256sum`
+  sha256 "35022b1b1c71ba21401f51f0d45c7576e995ca5a12bb0ccfd6fb8e2b8b207de2"
   license "MIT"
   head "https://github.com/czr/music-librarian.git", branch: "main"
 
